4b523f2cc7b182826677b299107041051569249c
Emmi Torstensson <emmi.torstensson@forefront.se>
2025-09-22 10:03:12 +0200

    Added back rule to check for FizzBuzz

--- fizzbuzz/src/rules/rules.ts
+++ fizzbuzz/src/rules/rules.ts
@@ -4,7 +4,7 @@ interface Rule {
 }
 
 export const ruleList: Rule[] = [
-    {condition: n => n % 3 === 0, output: "Fizz"},
+    {condition: n => n % 3 === 0 && n % 5 === 0 , output: "FizzBuzz"},
     {condition: n => n % 3 === 0, output: "Fizz"},
     {condition: n => n % 5 === 0, output: "Buzz"},
 ] 
